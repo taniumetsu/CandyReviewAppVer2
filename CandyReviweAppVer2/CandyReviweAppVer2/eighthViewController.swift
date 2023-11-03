1d352d77ce19b3dc1c9263effc37962f624eb7c5
Tani Umetsu <taniu@hawaii.edu>
2023-11-02 16:43:56 -1000

    completed part 3

---
+++ CandyReviweAppVer2/CandyReviweAppVer2/eighthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  eighthViewController.swift
+//  CandyReviweAppVer2
+//
+//  Created by Tani Umetsu on 11/2/23.
+//
+
+import UIKit
+
+class eighthViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePass: String?
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            if let imageName = imagePass {
+                imageView.image = UIImage(named: imageName)
+            }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
